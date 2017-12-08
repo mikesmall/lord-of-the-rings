21c55fa1be2daa8f8fa60a99a29aeaf031139004
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 12:34:36 -0500

    App.js imports and renders from Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ class Movie extends Component {
   render () {
     return (
       <div>
-        <h1>Back to the Future IV: President Biff</h1>
+        <h1>Back to the Future Part IV: President Biff</h1>
         <p>Runtime: 4h 37min</p>
       </div>
     )
