392677972adf13387a869439aa1769cb45b5780d
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 12:32:21 -0500

    h1 and p elements added to Movie class

--- src/Movie.js
+++ src/Movie.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Movie extends Component {
   render () {
     return (
-      // we'll add JSX here
+      <div>
+        <h1>Back to the Future IV: President Biff</h1>
+        <p>Runtime: 4h 37min</p>
+      </div>
     )
   }
 }
 
-export default Movie 
+export default Movie
